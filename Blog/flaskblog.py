866f7a7b71a1e1f54914742e8933c914eb731e18
Piotr Zacha <pzacha11@gmail.com>
2019-12-02 20:31:47 +0100

    Added content to home and about

--- Blog/flaskblog.py
+++ Blog/flaskblog.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Piotr Zacha',
+        'title': 'Post 1',
+        'content': 'Content 1',
+        'date': '02.12.2019'
+    },
+    {
+        'author': 'Jake Doe',
+        'title': 'Post 2',
+        'content': 'Content 2',
+        'date': '03.12.2019'
+    },
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 if __name__ == '__main__':
     app.run(debug=True)
